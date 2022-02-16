f5f0337a3101164658347a51c93c955a66596a1e
Jhonata <jhonatavbrg@gmail.com>
2022-02-16 13:16:22 -0300

    move func to another file

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,16 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 import time
-import re
+
+# from tech_news.aux_func import *
+from tech_news.aux_func import (
+    get_title,
+    get_timestamp,
+    get_writer,
+    get_shares,
+    get_comments,
+    get_summary,
+    get_sources,
+    get_categories,
+)
 
 
 def fetch(url):
     time.sleep(1)
     try:
         headers = {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5)" +
-            "AppleWebKit/537.36 (KHTML, like Gecko)" +
-            "Chrome/50.0.2661.102 Safari/537.36"
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5)"
+            + "AppleWebKit/537.36 (KHTML, like Gecko)"
+            + "Chrome/50.0.2661.102 Safari/537.36"
         }
         html = requests.get(url, headers=headers, timeout=3)
 
@@ -46,8 +57,8 @@ def scrape_next_page_link(html_content):
         button_pagination = soup.find(
             "a",
             {
-                "class": "tec--btn tec--btn--lg" +
-                " tec--btn--primary z--mx-auto z--mt-48"
+                "class": "tec--btn tec--btn--lg"
+                + " tec--btn--primary z--mx-auto z--mt-48"
             },
         )["href"]
 
@@ -57,111 +68,19 @@ def scrape_next_page_link(html_content):
         return None
 
 
-def parse_convert_string(string):
-    temp = re.findall(r'\d+', string)
-    list_num = list(map(int, temp))
-    result = list_num.pop()
-    return result
-
-
-def get_title(soup):
-    element = soup.find("h1", {"id": "js-article-title"})
-    if element:
-       return element.text
-
-    return None
-
-
-def get_timestamp(soup):
-    element = soup.find("time", {"id": "js-article-date"})
-    if element:
-        return element["datetime"]
-
-    return None
-
-
-def get_writer(soup):
-    element = soup.find("a", {"class": "tec--author__info__link"})
-    element_alt = soup.find("div", {"class": "tec--timestamp__item z--font-bold"})
-    element_leg = soup.find("div", {"id": "js-author-bar"})
-
-    if element: 
-        return element.text.strip()
-    elif element_alt: 
-        return element_alt.a.text.strip()
-    elif element_leg:
-        return element_leg.p.text.strip()
-
-    return None
-
-
-def get_shares(soup):
-    element = soup.find("div", {"class": "tec--toolbar__item"})
-    
-    if element:
-        element = parse_convert_string(element.text)
-        return element
-
-    return 0
-
-
-def get_comments(soup):
-    element = soup.find("button", {"id": "js-comments-btn"})
-
-    if element:
-        element = parse_convert_string(element.text)
-        return element
-
-    return 0
-
-def get_summary(soup):
-    element = soup.find("div", {"class": "tec--article__body"})
-
-    if element:
-        return element.p.text
-
-    return None
-
-def get_sources(soup):
-    elements = soup.find("div", {"class": "z--mb-16"})
-    list_sources = []
-
-    if elements:
-        for element in elements.div:
-            list_sources.append(element.text.strip())
-
-    list_sources = list(filter(lambda x: x != "", list_sources))
-
-    return list_sources
-
-
-def get_categories(soup):
-    elements = soup.find("div", {"id": "js-categories"})
-    list_categories = []
-
-    if elements:
-        for element in elements:
-            list_categories.append(element.text.strip()) 
-
-    list_categories = list(filter(lambda x: x != "", list_categories))
-
-    return list_categories
-
-
 def scrape_noticia(html_content):
-    #links = scrape_novidades(html_content)
+    # links = scrape_novidades(html_content)
     soup = BeautifulSoup(html_content, "html.parser")
     soup.prettify()
     links = []
     new_dict = dict()
-    
-    links.append(soup.find("link", {"rel": "canonical"})['href'])
 
+    links.append(soup.find("link", {"rel": "canonical"})["href"])
 
     for link in links:
-        #html_page = fetch(link)
-        #soup = BeautifulSoup(html_page, "html.parser")
-        #soup.prettify()
+        # html_page = fetch(link)
+        # soup = BeautifulSoup(html_page, "html.parser")
+        # soup.prettify()
 
         new_dict["url"] = link
         new_dict["title"] = get_title(soup)
@@ -172,7 +91,6 @@ def scrape_noticia(html_content):
         new_dict["summary"] = get_summary(soup)
         new_dict["sources"] = get_sources(soup)
         new_dict["categories"] = get_categories(soup)
-
 
     return new_dict
 
