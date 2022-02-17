7dfe141ee97c29007ae5f215057caa1736b59ab2
Jhonata <jhonatavbrg@gmail.com>
2022-02-17 15:25:12 -0300

    add req 06

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,15 @@
-# Requisito 6
+from tech_news.database import search_news
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    list_search = search_news({"title": {"$regex": title, "$options": "i"}})
+
+    list_tuple = list()
+
+    if list_search:
+        list_tuple.append((list_search[0]["title"], list_search[0]["url"]))
+
+    return list_tuple
 
 
 # Requisito 7
